d7719dc8d99929eadd862cb08f70554d838826b3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-11-11 15:56:08 +0100

    initialise a list of chars with icon data

--- MatchingPairs/Form1.cs
+++ MatchingPairs/Form1.cs
@@ -2,6 +2,12 @@ namespace MatchingPairs
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        List<char> icons = new List<char>()
+        {
+            '!', '!', 'N', 'N', ',', ',', 'k', 'k',
+            'b', 'b', 'v', 'v', 'w', 'w', 'z', 'z'
+        };
         public Form1()
         {
             InitializeComponent();
