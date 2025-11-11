72d2b60a5b93b9962746ab107323af882e46bc9e
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-11-11 16:08:03 +0100

    create AssignToSquare method for assigning icons to each label on the form

--- MatchingPairs/Form1.cs
+++ MatchingPairs/Form1.cs
@@ -3,14 +3,31 @@ namespace MatchingPairs
     public partial class Form1 : Form
     {
         Random random = new Random();
-        List<char> icons = new List<char>()
+        List<string> icons = new List<string>()
         {
-            '!', '!', 'N', 'N', ',', ',', 'k', 'k',
-            'b', 'b', 'v', 'v', 'w', 'w', 'z', 'z'
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
         };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AssignIconsToSquare()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null ) 
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
     }
 }
